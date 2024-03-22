c7dc231d6f7b05d8a39d3a3db3c9485740521130
jordan <jordan.jakisa@gmail.com>
2024-03-22 14:54:41 +0300

    🤝 Added the ability for people to upload their own books
    Signed-off-by: jordan <jordan.jakisa@gmail.com>

--- app/src/main/java/com/keru/novelly/ui/pages/uploadpage/UploadBookScreen.kt
+++ app/src/main/java/com/keru/novelly/ui/pages/uploadpage/UploadBookScreen.kt
@@ -1,24 +1,37 @@
 package com.keru.novelly.ui.pages.uploadpage
 
 import android.net.Uri
+import android.widget.Toast
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.scaleIn
+import androidx.compose.animation.scaleOut
 import androidx.compose.animation.slideInHorizontally
+import androidx.compose.animation.slideInVertically
 import androidx.compose.animation.slideOutVertically
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.outlined.ArrowBack
+import androidx.compose.material.icons.rounded.CheckCircle
 import androidx.compose.material3.Button
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ElevatedButton
+import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ExposedDropdownMenuBox
 import androidx.compose.material3.ExposedDropdownMenuDefaults
@@ -28,15 +41,19 @@ import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableFloatStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.compose.ui.window.Dialog
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import coil.compose.AsyncImage
@@ -48,7 +65,9 @@ fun UploadBookScreen(
     navController: NavController,
     vm: UploadBookViewModel = hiltViewModel()
 ) {
-    Column(modifier = Modifier.fillMaxSize()) {
+    Column(
+        modifier = Modifier.fillMaxSize()
+    ) {
 
         var isExpanded by rememberSaveable { mutableStateOf(false) }
         val scrollState = rememberScrollState()
@@ -57,12 +76,33 @@ fun UploadBookScreen(
         var author by rememberSaveable { mutableStateOf("") }
         var genre by rememberSaveable { mutableStateOf("") }
         var imageUri by rememberSaveable { mutableStateOf(Uri.EMPTY) }
-        var bookRating by rememberSaveable { mutableFloatStateOf(0f) }
+        var bookRating by rememberSaveable { mutableStateOf("0.0") }
+        var bookUri by rememberSaveable { mutableStateOf(Uri.EMPTY) }
+        val uiState = vm.uiState
+        val context = LocalContext.current
+
+        LaunchedEffect(key1 = uiState.message) {
+            uiState.message?.let {
+                if (it.isNotEmpty()) Toast.makeText(context, it, Toast.LENGTH_SHORT).show()
+            }
+        }
+        LaunchedEffect(key1 = uiState.error) {
+            uiState.error?.let {
+                if (it.isNotEmpty()) Toast.makeText(context, it, Toast.LENGTH_SHORT).show()
+            }
+        }
 
         val pickImage =
             rememberLauncherForActivityResult(contract = ActivityResultContracts.GetContent()) { uri ->
                 uri?.let {
                     imageUri = it
+                }
+            }
+
+        val pickBook =
+            rememberLauncherForActivityResult(contract = ActivityResultContracts.GetContent()) { uri ->
+                uri?.let {
+                    bookUri = it
                 }
             }
 
@@ -72,7 +112,10 @@ fun UploadBookScreen(
                 Text(text = "Upload book")
             }, navigationIcon = {
                 IconButton(onClick = { navController.navigateUp() }) {
-                    Icon(imageVector = Icons.AutoMirrored.Outlined.ArrowBack, contentDescription = "")
+                    Icon(
+                        imageVector = Icons.AutoMirrored.Outlined.ArrowBack,
+                        contentDescription = ""
+                    )
                 }
             })
         Column(
@@ -107,11 +150,14 @@ fun UploadBookScreen(
                 modifier = Modifier.fillMaxWidth()
             )
             OutlinedTextField(
-                value = bookRating.toString(),
-                onValueChange = { bookRating = it.toFloat() },
+                value = bookRating,
+                onValueChange = { bookRating = it },
                 label = { Text(text = "Book rating") },
                 placeholder = { Text(text = "e.g 4.5 ") },
                 modifier = Modifier.fillMaxWidth(),
+                keyboardOptions = KeyboardOptions(
+                    keyboardType = KeyboardType.Number
+                )
             )
 
             ExposedDropdownMenuBox(
@@ -150,17 +196,44 @@ fun UploadBookScreen(
                     }
                 }
             }
-            ElevatedButton(onClick = {
-                pickImage.launch("image/*")
-            }, modifier = Modifier.fillMaxWidth()) {
-                Text(text = "Click to select a cover image for the book")
+
+            Row(
+                horizontalArrangement = Arrangement.spacedBy(16.dp),
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                ElevatedButton(onClick = {
+                    pickBook.launch("application/pdf")
+                }, modifier = Modifier.weight(1f)) {
+                    Text(text = "Pick book PDF")
+                    Spacer(modifier = Modifier.width(8.dp))
+                    AnimatedVisibility(
+                        visible = bookUri.toString().isNotEmpty(),
+                        enter = fadeIn() + scaleIn(),
+                        exit = fadeOut() + scaleOut()
+                    ) {
+                        Icon(
+                            imageVector = Icons.Rounded.CheckCircle,
+                            contentDescription = "",
+                            modifier = Modifier.size(24.dp),
+                            tint = Color.Green
+                        )
+                    }
+
+                }
+                ElevatedButton(onClick = {
+                    pickImage.launch("image/*")
+                }, modifier = Modifier.weight(1f)) {
+                    Text(text = "Pick image")
+                }
+
             }
+
             AnimatedVisibility(
                 visible = imageUri.toString().isNotEmpty(),
                 enter = slideInHorizontally { it },
                 exit = slideOutVertically { it }) {
                 Column {
-                    Text(text = "Selected image")
+                    Text(text = "Selected image", fontSize = 12.sp, modifier = Modifier.alpha(.75f))
                     AsyncImage(
                         model = imageUri,
                         contentDescription = "",
@@ -170,16 +243,40 @@ fun UploadBookScreen(
                 }
             }
 
+
+
             Button(
                 onClick = {
-                    vm.uploadBook(title, description, author, genre, imageUri, bookRating)
+                    vm.uploadBook(
+                        title,
+                        description,
+                        author,
+                        genre,
+                        imageUri,
+                        bookRating.toFloat().coerceAtMost(5f),
+                        bookUri
+                    ) {
+                        navController.navigateUp()
+                    }
                 },
                 modifier = Modifier.fillMaxWidth(),
-                enabled = title.isNotEmpty() && description.isNotEmpty() && author.isNotEmpty() && genre.isNotEmpty() && imageUri.toString()
+                enabled = title.isNotEmpty() && description.isNotEmpty() && author.isNotEmpty() && genre.isNotEmpty() && bookUri.toString()
                     .isNotEmpty()
             ) {
                 Text(text = "Save")
             }
         }
+
+        AnimatedVisibility(
+            visible = vm.uiState.isLoading,
+            enter = slideInVertically { it },
+            exit = slideOutVertically { it }) {
+            Dialog(onDismissRequest = { /*TODO*/ }) {
+                ElevatedCard {
+                    Text(text = "Uploading ... ", modifier = Modifier.padding(16.dp))
+                }
+            }
+        }
+
     }
 }
